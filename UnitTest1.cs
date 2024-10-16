95822b8c2e7994aeb6c0ebc84bd53b90eacfe9b9
Ewa <ewadobroc@yahoo.com>
2024-10-16 16:00:23 +0100

    added comments

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,31 +13,31 @@ namespace AiraloAutomationTests
         [SetUp]
         public void Setup()
         {
+
         }
 
         [Test]
         public async Task Test1()
         {
-            //Playwright instance
-
-            //using var playwright = await Playwright.CreateAsync();
-
-            //Browser
-            //await using var browser = await playwright.Chromium.LaunchAsync();
-
-            //page
-            // var page = await browser.NewPageAsync();
+           //Navigating to website
             await Page.GotoAsync("https://www.airalo.com/");
+
+            //Change the website currency from GBP to USD
             await Page.GetByTestId("£ GBP-header-language").ClickAsync();
             await Page.GetByTestId("currencies-list-search-bar").ClickAsync();
             await Page.GetByTestId("currencies-list-search-bar").FillAsync("usd");
             await Page.GetByTestId("USD-currency-select").ClickAsync();
             await Page.GetByTestId("UPDATE-button").ClickAsync();
+
+            //Search for Japan in search bar
             await Page.GetByTestId("search-input").ClickAsync();
             await Page.GetByTestId("search-input").FillAsync("Japan");
             await Page.Locator("span").Filter(new() { HasTextRegex = new Regex("^Japan$") }).ClickAsync();
+
+            //Select the relevant Japan eSIM data package
             await Page.GetByRole(AriaRole.Link, new() { Name = "Moshi Moshi Moshi Moshi  COVERAGE Japan  DATA 1 GB  VALIDITY 7 Days PRICE $4.50" }).ClickAsync();
 
+            //Verify eSIM details
             await Expect(Page.Locator("[data-testid='sim-detail-operator-title']")).ToContainTextAsync("Moshi Moshi");
             await Expect(Page.Locator("[data-testid='sim-detail-info-list']")).ToContainTextAsync("COVERAGE \r\n        Japan\r\n          DATA \r\n        1 GB\r\n            VALIDITY \r\n        7 Days\r\n         \r\n    \r\n    \r\n PRICE \r\n        $4.50 USD");
 
