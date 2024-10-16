82f6d83efa7ec40b9bb731388ece9b5274ed017a
Ewa <ewadobroc@yahoo.com>
2024-10-16 15:11:40 +0100

    tests added

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,28 +1,54 @@
-namespace Airalo
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+
+
+
+namespace AiraloAutomationTests
 {
-    [Parallelizable(ParallelScope.Self)]
-    [TestFixture]
     public class Tests : PageTest
     {
-        [Test]
-        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+        [SetUp]
+        public void Setup()
         {
-            await Page.GotoAsync("https://playwright.dev");
+        }
 
-            // Expect a title "to contain" a substring.
-            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+        [Test]
+        public async Task Test1()
+        {
+            //Playwright instance
 
-            // create a locator
-            var getStarted = Page.Locator("text=Get Started");
+            //using var playwright = await Playwright.CreateAsync();
 
-            // Expect an attribute "to be strictly equal" to the value.
-            await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+            //Browser
+            //await using var browser = await playwright.Chromium.LaunchAsync();
 
-            // Click the get started link.
-            await getStarted.ClickAsync();
+            //page
+            // var page = await browser.NewPageAsync();
+            await Page.GotoAsync("https://www.airalo.com/");
+            await Page.GetByTestId("£ GBP-header-language").ClickAsync();
+            await Page.GetByTestId("currencies-list-search-bar").ClickAsync();
+            await Page.GetByTestId("currencies-list-search-bar").FillAsync("usd");
+            await Page.GetByTestId("USD-currency-select").ClickAsync();
+            await Page.GetByTestId("UPDATE-button").ClickAsync();
+            await Page.GetByTestId("search-input").ClickAsync();
+            await Page.GetByTestId("search-input").FillAsync("Japan");
+            await Page.Locator("span").Filter(new() { HasTextRegex = new Regex("^Japan$") }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Moshi Moshi Moshi Moshi  COVERAGE Japan  DATA 1 GB  VALIDITY 7 Days PRICE $4.50" }).ClickAsync();
 
-            // Expects the URL to contain intro.
-            await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+            await Expect(Page.Locator("[data-testid='sim-detail-operator-title']")).ToContainTextAsync("Moshi Moshi");
+            await Expect(Page.Locator("[data-testid='sim-detail-info-list']")).ToContainTextAsync("COVERAGE \r\n        Japan\r\n          DATA \r\n        1 GB\r\n            VALIDITY \r\n        7 Days\r\n         \r\n    \r\n    \r\n PRICE \r\n        $4.50 USD");
+
+
+
+
+
+
+
+
         }
     }
 }
+
